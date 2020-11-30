26ae3eb5c78989de185121379e49d68b60bd05d6
Damian Śniatecki <damian.sniatecki@axisdata.net>
2020-11-30 19:39:49 +0100

    Refactor

--- reviewer-songs.js
+++ reviewer-songs.js
@@ -1,5 +1,14 @@
+const websiteSettings = {
+    isSettingsSelectorOpen: false,
+    reviewsColor: "#000000",
+    reviewsFontSize: 16,
+    reviewsOpacity: 97
+}
+
 const songReviews = [
     {
+        "id": "123123213",
+        "isYoutubePlayerOpen": false,
         "song": {
             "name": "505",
             "artist": "Arctic Monkeys",
@@ -13,6 +22,8 @@ const songReviews = [
         "review": "Wherever they turn, irony dissolves and Turner, acting out lyrics like “I lost my train of thought” or “she held me very tightly” with am-dram theatricality, can’t help but be a real rock star even as he plays with the role. In the end, every side to the band – funk troupe, garage punks, aloof posers, heartfelt songwriters – coheres on the closing R U Mine?, a diamond anthem that tightens and collapses at all the right moments. Now armed with what is essentially a greatest-hits set, Arctic Monkeys have become a band who can do everything – it might be a construct, but it feels so real."
     },
     {
+        "id": "12376445213",
+        "isYoutubePlayerOpen": false,
         "song": {
             "name": "Do I wanna know",
             "artist": "Arctic Monkeys",
@@ -28,72 +39,185 @@ const songReviews = [
             "Much better is when their chops are transmuted into pure energy: Brianstorm, Don’t Sit Down ’Cause I’ve Moved Your Chair and View from the Afternoon swerve too fast through their corners and are all the better for it. The drummer Matt Helders, a mere timekeeper during the Tranquility Base material, is transformed into a dynamo; with a spotlight behind him, he becomes a tub-thumping Godzilla on the rear wall."
     },
     {
-        song: {
-            album: "Favourite Worst Nightmare",
-            artist: " Arctic Monkeys",
-            name: "Fluorescent Adolescent ",
-            youtubeUrl: "https://www.youtube.com/embed/ma9I9VBKPiw"
+        "id": "27657653213",
+        "isYoutubePlayerOpen": false,
+        "song": {
+            "album": "Hybrid Theory",
+            "artist": "Linkin Park",
+            "name": "Numb",
+            "youtubeUrl": "https://www.youtube.com/embed/kXYiU_JCYtU"
         },
-        review: "Great Song !!!",
-        reviewer: {
-            email: "awdas@gmail.com",
-            nickname: "Kowalkiewicz"
+        "review": "In May of 2019, an anonymous outfit known only as SAULT released an album of tasteful soul-funk with a scratchy DIY veneer that sounded like an Instagram-filtered reunion of ESG. Biography-shy musicians that bring a retro sensibility to the music of late-1970s, early-’80s roller rinks and B-boys aren’t unusual, from the action-packed exuberance of the Go! Team in the 2000s to the falsetto-streaked brooding of Jungle in the last decade. SAULT, though, were unusually prolific, and they had something to say. With lyrics foregrounding Black identity, June’s UNTITLED (Black Is) seemed like a fitting soundtrack for this summer of collective action against police violence and systemic racism.",
+        "reviewer": {
+            "email": "reviwer2222@gmail.com",
+            "nickname": "SuperReviewer"
+        },
+    },
+    {
+        "id": "12319923213",
+        "isYoutubePlayerOpen": false,
+        "song": {
+            "album": "Hybrid Theory",
+            "artist": "Linkin Park",
+            "name": "Castle of glass",
+            "youtubeUrl": "https://www.youtube.com/embed/ScNNfyq3d_w"
+        },
+        "review": "Wherever they turn, irony dissolves and Turner, acting out lyrics like “I lost my train of thought” or “she held me very tightly” with am-dram theatricality, can’t help but be a real rock star even as he plays with the role. In the end, every side to the band – funk troupe, garage punks, aloof posers, heartfelt songwriters – coheres on the closing R U Mine?, a diamond anthem that tightens and collapses at all the right moments. Now armed with what is essentially a greatest-hits set, Arctic Monkeys have become a band who can do everything – it might be a construct, but it feels so real.",
+        "reviewer": {
+            "email": "superbear@gmail.com",
+            "nickname": "Bear123"
+        },
+    },
+    {
+        "id": "1777123213",
+        "isYoutubePlayerOpen": false,
+        "song": {
+            "album": "Favourite Worst Nightmare",
+            "artist": " Arctic Monkeys",
+            "name": "Fluorescent Adolescent ",
+            "youtubeUrl": "https://www.youtube.com/embed/ma9I9VBKPiw"
+        },
+        "review": "Great Song !!!",
+        "reviewer": {
+            "email": "awdas@gmail.com",
+            "nickname": "Kowalkiewicz"
         },
     }
 ]
 
+function showWebsiteSettings(shouldShowOpacity) {
+    document.getElementById("websiteSettingsSelectors").innerHTML = `
+        <p><label>Reviews theme color: <input id="reviewsColor" type="color" value="${websiteSettings.reviewsColor}"></label></p>
+        <p><label>Reviews font size: <input id="reviewsFontSize" type="range" min="12" max="28" value="${websiteSettings.reviewsFontSize}"></label>
+            <output id="reviewsFontSizeValue">${websiteSettings.reviewsFontSize}px</output></p>
+    `
+    document.getElementById("reviewsColor").addEventListener("change", event => {
+        websiteSettings.reviewsColor = event.target.value
+        loadReviews()
+    })
+    document.getElementById("reviewsFontSize").addEventListener("change", event => {
+        const newFontSize = event.target.value;
+        websiteSettings.reviewsFontSize = newFontSize
+        document.getElementById("reviewsFontSizeValue").innerText = `${newFontSize}px`
+        loadReviews()
+    })
+    if (shouldShowOpacity) {
+        document.getElementById("websiteSettingsSelectors").innerHTML += ` 
+        <p>
+        <label>Reviews opacity: <input id="reviewsOpacity" type="range" min="80" max="100" value="${websiteSettings.reviewsOpacity}"> <output id="reviewsOpacityValue">${websiteSettings.reviewsOpacity}%</output>
+            </label>
+        </p>
+        `
+        document.getElementById("reviewsOpacity").addEventListener("change", event => {
+            const newOpacity = event.target.value;
+            websiteSettings.reviewsOpacity = newOpacity
+            document.getElementById("reviewsOpacityValue").innerText = `${newOpacity}%`
+            loadReviews()
+        })
+    }
+}
+
+function manageWebsiteSettings(event) {
+    if (websiteSettings.isSettingsSelectorOpen) {
+        if (event.altKey) {
+            setTimeout(() => {
+                document.getElementById("websiteSettingsSelectors").innerHTML = ""
+                websiteSettings.isSettingsSelectorOpen = false;
+            }, 2000);
+        } else {
+            document.getElementById("websiteSettingsSelectors").innerHTML = "";
+            websiteSettings.isSettingsSelectorOpen = false;
+        }
+    } else {
+        showWebsiteSettings(event.shiftKey)
+        websiteSettings.isSettingsSelectorOpen = true;
+    }
+}
+
 function loadWebsite() {
+    document.getElementById("cursorPosition").addEventListener("mousemove", e => {
+        document.getElementById("cursorPosition").innerHTML =
+            `<p>
+                    <strong>Screen X/Y:</strong> ${e.screenX}, ${e.screenY}
+                    <strong>Client X/Y:</strong> ${e.clientX}, ${e.clientY}
+             </p>
+            `;
+    })
+    document.getElementById("cursorPosition").addEventListener("mouseleave", e => {
+        document.getElementById("cursorPosition").innerHTML = ``
+    })
+
     document.getElementById("addNewReviewForm").addEventListener("submit", addNewReviewFromFromEvent)
+    document.getElementById("websiteSettingsSection").addEventListener("click", manageWebsiteSettings)
     loadReviews();
 }
 
 function loadReviews() {
-    const reviewDivs = songReviews
-        .map(review => createReviewElement(review))
-        .join("")
-    document.getElementById("reviews").innerHTML = `<div>${reviewDivs}</div>`
+    const reviewDiv = document.createElement("div");
+    for (const review of songReviews) {
+        reviewDiv.append(createReviewElement(review))
+    }
+    document.getElementById("reviews").innerHTML = reviewDiv.textContent
+    for (const review of songReviews) {
+        document.getElementById(review.id).addEventListener("click", (e) => manageYoutubePlayer(review))
+    }
 }
 
-const addNewReviewToWebsite = (newReview) =>
-    document.getElementById("reviews").innerHTML += createReviewElement(newReview)
-
-
 function addNewReviewFromFromEvent(event) {
-    const formElements = event.target.elements;
+    const formElements = document.forms.namedItem("newReviewForm").elements;
+    console.log(formElements)
     const newReview = {
+        "id": formElements.namedItem("songYoutubeUrl").value +
+            songReviews.length + formElements.namedItem("songName").value,
+        "isYoutubePlayerOpen": false,
         "song": {
-            "name": formElements["songName"].value,
-            "artist": formElements["artistName"].value,
-            "youtubeUrl": formElements["songYoutubeUrl"].value,
-            "album": formElements["albumName"].value,
+            "name": formElements.namedItem("songName").value,
+            "artist": formElements.namedItem("artistName").value,
+            "youtubeUrl": formElements.namedItem("songYoutubeUrl").value,
+            "album": formElements.namedItem("albumName").value,
         },
         "reviewer": {
-            "nickname": formElements["reviewerNickname"].value,
-            "email": formElements["reviewerEmail"].value
+            "nickname": formElements.namedItem("reviewerNickname").value,
+            "email": formElements.namedItem("reviewerEmail").value
         },
-        "review": formElements["review"].value
+        "review": formElements.namedItem("review").value
     }
     event.preventDefault()
     event.target.reset()
     songReviews.push(newReview)
-    addNewReviewToWebsite(newReview)
+    loadReviews()
     console.log(`New review was added!`)
     console.log(newReview)
 }
 
 
-function createReviewElement(review) {
-    const {name, artist, youtubeUrl, album} = review.song;
-    const {nickname, email} = review.reviewer;
+function manageYoutubePlayer(review) {
+    if (review.isYoutubePlayerOpen) {
+        document.getElementById(`player-${review.id}`).innerHTML = ``;
+        review.isYoutubePlayerOpen = false;
+    } else {
+        document.getElementById(`player-${review.id}`).innerHTML = `
+        <iframe width="600" height="300" src="${review.song.youtubeUrl}"></iframe>
+        `;
+        review.isYoutubePlayerOpen = true;
+    }
+}
+
+function createReviewElement(reviewObj) {
+    const {name, artist, album} = reviewObj.song;
+    const {nickname, email} = reviewObj.reviewer;
+    const style = `color: ${websiteSettings.reviewsColor}; font-size: ${websiteSettings.reviewsFontSize}px;`
     return `
-        <section>
+        <section id="${reviewObj.id}" style="opacity: ${websiteSettings.reviewsOpacity}%; cursor: pointer;">
             <h3>${name}</h3>
-            <p>Artist : ${artist}</p>
-            <p>Album : ${album}</p>
-            <iframe width="600" height="300" src="${youtubeUrl}"></iframe>
-            <p>Review : ${review.review}</p>
-            <p>Nickname: ${nickname}</p>
-            <p>Reviewer email : ${email}</p>
+            <p><strong style="${style}">Artist:</strong> ${artist}</p>
+            <p><strong style="${style}">Album:</strong> ${album}</p>
+            <div id="player-${reviewObj.id}"></div>
+            <p><strong style="${style}">Review:</strong> ${reviewObj.review}</p>
+            <p><strong style="${style}">Nickname:</strong> ${nickname}</p>
+            <p><strong style="${style}">Reviewer email:</strong> ${email}</p>
         </section>
         `
 }
+
+
